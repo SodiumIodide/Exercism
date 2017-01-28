20cc4abeec5c9e1613befefe984da390c1eab55d
SodiumIodide <corey.s@gmx.us>
2017-01-28 13:56:12 -0700

    Series problem

---
+++ python/series/series.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''Series Problem'''
+
+import argparse
+
+def slices(inpstr, size):
+    '''Return a "rolling" or "sliding" operator on input string'''
+    if size == 0 or size > len(inpstr):
+        raise ValueError("Invalid size input")
+    if not inpstr.isdigit():
+        raise ValueError("Invalid string input")
+    return [list(map(lambda x: int(x), inpstr[i:i+size]))
+            for i in range(len(inpstr) - (size - 1))]
+
+def main():
+    '''Main wrapper'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('string', help='string of numbers to oeprate on')
+    parser.add_argument('length', type=int, help='size of window')
+    args = parser.parse_args()
+    try:
+        parts = slices(args.string, args.length)
+        for part in parts:
+            print(''.join(str(x) for x in part))
+    except ValueError:
+        print("Invalid input")
+
+if __name__ == '__main__':
+    main()
