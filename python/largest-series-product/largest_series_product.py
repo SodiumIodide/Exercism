d384d4eac61485e341af1c46369c5e799989e24c
SodiumIodide <corey.s@gmx.us>
2017-01-30 19:31:01 -0700

    Largest Series Product + refinement to Series problem

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,17 +1,36 @@
 #!usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''Series Problem'''
+
+'''Largest Series Product Problem'''
 
 import argparse
 
+# Reusing code from a previous problem: series
 def slices(inpstr, size):
     '''Return a "rolling" or "sliding" operator on input string'''
-    if size == 0 or size > len(inpstr):
+    if size <= 0 or size > len(inpstr):
         raise ValueError("Invalid size input")
     if not inpstr.isdigit():
         raise ValueError("Invalid string input")
     return [list(map(lambda x: int(x), inpstr[i:i+size]))
             for i in range(len(inpstr) - (size - 1))]
+
+def largest_product(inpstr, size):
+    '''Return the largest product of a rolling operator over a digital series'''
+    if size == 0:
+        return 1
+    try:
+        series = slices(inpstr, size)
+    except ValueError as vale:
+        raise vale
+    products = []
+    for chain in series:
+        # Initial value of unity
+        product = 1
+        for digit in chain:
+            product *= digit
+        products.append(product)
+    return max(products)
 
 def main():
     '''Main wrapper'''
@@ -20,9 +39,7 @@ def main():
     parser.add_argument('length', type=int, help='size of window')
     args = parser.parse_args()
     try:
-        parts = slices(args.string, args.length)
-        for part in parts:
-            print(''.join(str(x) for x in part))
+        print(largest_product(args.string, args.length))
     except ValueError:
         print("Invalid input")
 
